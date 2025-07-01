09fed94b20e09e7970b0e59b117ff5c07e200771
iamharshtambe <harshtambe.dev@gmail.com>
2025-07-01 12:11:36 +0530

    add optional chaining

--- optional-chaining.js
+++ optional-chaining.js
@@ -5,6 +5,7 @@ const book = getBook(3);
 /*
 ✅ Optional Chaining (?.): Safely access nested properties without throwing an error if any part is undefined or null
 */
+
 function getTotalReviewsCount(book) {
    const goodreads = book.reviews?.goodreads?.reviewsCount;
    const librarything = book.reviews?.librarything?.reviewsCount ?? 0;
