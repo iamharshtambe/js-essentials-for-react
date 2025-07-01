57400ac6a9dd1b6491edea2c4c0403af053f6c53
iamharshtambe <harshtambe.dev@gmail.com>
2025-07-01 17:37:13 +0530

    add data

---
+++ data/extraBook.js
@@ -0,0 +1,32 @@
+export const extraBook = {
+   id: 6,
+   title: 'To Kill a Mockingbird',
+   publicationDate: '1960-07-11',
+   author: 'Harper Lee',
+   genres: [
+      'classic',
+      'historical fiction',
+      'coming-of-age',
+      'drama',
+      'literature',
+   ],
+   hasMovieAdaptation: true,
+   pages: 336,
+   translations: {
+      spanish: 'Matar a un ruiseñor',
+      french: 'Ne tirez pas sur l’oiseau moqueur',
+      italian: 'Il buio oltre la siepe',
+   },
+   reviews: {
+      goodreads: {
+         rating: 4.28,
+         ratingsCount: 4800000,
+         reviewsCount: 95000,
+      },
+      librarything: {
+         rating: 4.3,
+         ratingsCount: 120000,
+         reviewsCount: 2100,
+      },
+   },
+};
